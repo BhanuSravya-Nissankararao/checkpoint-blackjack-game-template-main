e84f8361e323aa111124f6d08bcf4c12ea21b3ad
BhanuSravya-Nissankararao <121043749+BhanuSravya-Nissankararao@users.noreply.github.com>
2022-12-29 05:01:28 -1200

    changed

--- script.js
+++ script.js
@@ -10,18 +10,27 @@
     const packObj = {}
 
     // write your code here
-    packArr=["Ace of Hearts" , "2 of Hearts" , "3 of Hearts" , "4 of Hearts" , "5 of Hearts" , "6 of Hearts" , "7 of Hearts" , "8 of Hearts" , "9 of Hearts" , "10 of Hearts" , "Jack of Hearts" , "King of Hearts" , "Queen of Hearts",
-            "Ace of Diamonds" , "2 of Diamonds" , "3 of Diamonds" , "4 of Diamonds" , "5 of Diamonds" , "6 of Diamonds" , "7 of Diamonds" , "8 of Diamonds" , "9 of Diamonds" , "10 of Diamonds" , "Jack of Diamonds" , "King of Diamonds" , "Queen of Diamonds",
-            "Ace of Spades" , "2 of Spades" , "3 of Spades" , "4 of Spades" , "5 of Spades" , "6 of Spades" , "7 of Spades" , "8 of Spades" , "9 of Spades" , "10 of Spades" , "Jack of Spades" , "King of Spades" , "Queen of Spades",
-            "Ace of Clubs" , "2 of Clubs" , "3 of Clubs" , "4 of Clubs" , "5 of Clubs" , "6 of Clubs" , "7 of Clubs" , "8 of Clubs" , "9 of Clubs" , "10 of Clubs" , "Jack of Clubs" , "King of Clubs" , "Queen of Clubs"
-            ];
+    // packArr=["Ace of Hearts" , "2 of Hearts" , "3 of Hearts" , "4 of Hearts" , "5 of Hearts" , "6 of Hearts" , "7 of Hearts" , "8 of Hearts" , "9 of Hearts" , "10 of Hearts" , "Jack of Hearts" , "King of Hearts" , "Queen of Hearts",
+    //         "Ace of Diamonds" , "2 of Diamonds" , "3 of Diamonds" , "4 of Diamonds" , "5 of Diamonds" , "6 of Diamonds" , "7 of Diamonds" , "8 of Diamonds" , "9 of Diamonds" , "10 of Diamonds" , "Jack of Diamonds" , "King of Diamonds" , "Queen of Diamonds",
+    //         "Ace of Spades" , "2 of Spades" , "3 of Spades" , "4 of Spades" , "5 of Spades" , "6 of Spades" , "7 of Spades" , "8 of Spades" , "9 of Spades" , "10 of Spades" , "Jack of Spades" , "King of Spades" , "Queen of Spades",
+    //         "Ace of Clubs" , "2 of Clubs" , "3 of Clubs" , "4 of Clubs" , "5 of Clubs" , "6 of Clubs" , "7 of Clubs" , "8 of Clubs" , "9 of Clubs" , "10 of Clubs" , "Jack of Clubs" , "King of Clubs" , "Queen of Clubs"
+    //         ];
 
-    packObj={"Ace of Hearts":1 , "2 of Hearts":2 , "3 of Hearts":3 , "4 of Hearts":4 , "5 of Hearts":5 , "6 of Hearts":6 , "7 of Hearts":7 , "8 of Hearts":8 , "9 of Hearts":9 , "10 of Hearts":10 , "Jack of Hearts":11 , "King of Hearts":12 , "Queen of Hearts":13,
-            "Ace of Diamonds":1 , "2 of Diamonds":2 , "3 of Diamonds":3 , "4 of Diamonds":4 , "5 of Diamonds":5 , "6 of Diamonds":6 , "7 of Diamonds":7 , "8 of Diamonds":8 , "9 of Diamonds":9 , "10 of Diamonds":10 , "Jack of Diamonds":11 , "King of Diamonds":12 , "Queen of Diamonds":13,
-            "Ace of Spades":1 , "2 of Spades":2 , "3 of Spades":3 , "4 of Spades":4 , "5 of Spades":5 , "6 of Spades":6 , "7 of Spades":7 , "8 of Spades":8 , "9 of Spades":9 , "10 of Spades":10 , "Jack of Spades":11 , "King of Spades":12 , "Queen of Spades":13,
-            "Ace of Clubs":1 , "2 of Clubs":2 , "3 of Clubs":3 , "4 of Clubs":4 , "5 of Clubs":5 , "6 of Clubs":6 , "7 of Clubs":7 , "8 of Clubs":8 , "9 of Clubs":9 , "10 of Clubs":10 , "Jack of Clubs":11 , "King of Clubs":12 , "Queen of Clubs":13
-
-            };
+    // packObj={"Ace of Hearts":1 , "2 of Hearts":2 , "3 of Hearts":3 , "4 of Hearts":4 , "5 of Hearts":5 , "6 of Hearts":6 , "7 of Hearts":7 , "8 of Hearts":8 , "9 of Hearts":9 , "10 of Hearts":10 , "Jack of Hearts":11 , "King of Hearts":12 , "Queen of Hearts":13,
+    //         "Ace of Diamonds":1 , "2 of Diamonds":2 , "3 of Diamonds":3 , "4 of Diamonds":4 , "5 of Diamonds":5 , "6 of Diamonds":6 , "7 of Diamonds":7 , "8 of Diamonds":8 , "9 of Diamonds":9 , "10 of Diamonds":10 , "Jack of Diamonds":11 , "King of Diamonds":12 , "Queen of Diamonds":13,
+    //         "Ace of Spades":1 , "2 of Spades":2 , "3 of Spades":3 , "4 of Spades":4 , "5 of Spades":5 , "6 of Spades":6 , "7 of Spades":7 , "8 of Spades":8 , "9 of Spades":9 , "10 of Spades":10 , "Jack of Spades":11 , "King of Spades":12 , "Queen of Spades":13,
+    //         "Ace of Clubs":1 , "2 of Clubs":2 , "3 of Clubs":3 , "4 of Clubs":4 , "5 of Clubs":5 , "6 of Clubs":6 , "7 of Clubs":7 , "8 of Clubs":8 , "9 of Clubs":9 , "10 of Clubs":10 , "Jack of Clubs":11 , "King of Clubs":12 , "Queen of Clubs":13
+    //         };
+    for(var i=0;i<suits.length;i++){
+        var k=1;
+            for(var j=0;j<values.length;j++){
+                var s='';
+                s+=values[j]+' of '+suits[i];
+                packArr.push(s)
+                packObj[s]=k;
+                k+=1
+            }
+        }
 
 
     if(!asArray){
